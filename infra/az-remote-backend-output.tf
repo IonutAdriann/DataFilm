4e9d80116eea4a924bf46fbd78fe004f8fee739a
IonutAdriann <ionutrotaru077@gmail.com>
2023-05-31 19:40:23 +0200

    fix: solving the output.tf issue

--- infra/az-remote-backend-output.tf
+++ infra/az-remote-backend-output.tf
@@ -1,9 +1,11 @@
 output "terraform_state_resource_group_name" {
-  value = datafilm_resource_group.state-rg.name
+  value = azurerm_resource_group.state-rg.name
 }
+
 output "terraform_state_storage_account" {
-  value = datafilm_storage_account.state-sta.name
+  value = azurerm_storage_account.state-sta.name
 }
+
 output "terraform_state_storage_container_core" {
-  value = datafilm_storage_container.core-container.name
+  value = azurerm_storage_container.core-container.name
 }
