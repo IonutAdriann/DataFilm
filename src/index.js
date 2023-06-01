113caeb449e69006387f81b0c8f559e729f57705
Andronache George Eduard <42812475+neutralfusion@users.noreply.github.com>
2023-06-01 05:56:18 +0200

    Feat/redux setup (#9)
    * feat: fixing the dependecies
    
    * Feat: Implementing the Navbar
    
    * feat: implementing redux and setting up tmdb

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import ToggleColorModeProvider from './utils/ToggleColorMode';
 import App from './components/App';
-
-const theme = createTheme({});
+import store from './app/store';
+import './index.css';
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </ThemeProvider>,
-
-    document.getElementById('root'));
+  <Provider store={store}>
+    <ToggleColorModeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ToggleColorModeProvider>
+  </Provider>,
+  document.getElementById('root'),
+);
