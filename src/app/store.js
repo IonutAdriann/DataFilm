22089554b007c90652594964dbf1d3908787b4d7
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2023-06-05 16:25:15 +0200

    Feat/fix middleware (#22)
    * feat: adding last changes to the repo
    
    * fix: adding middleware for fixing the tokens

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { tmdbApi } from '../services/TMDB';
 import genreOrCategoryReducer from '../features/currentGenreOrCategory';
 import userReducer from '../features/auth';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
     currentGenreOrCategory: genreOrCategoryReducer,
     user: userReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tmdbApi.middleware),
 });
+
+setupListeners(store.dispatch);
+
+export default store;
+
+
+
